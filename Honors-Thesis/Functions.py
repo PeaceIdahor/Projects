62afdc0f14ad9c2466d928bba21efa597b93731b
PeaceIdahor <70335777+PeaceIdahor@users.noreply.github.com>
2023-02-28 17:26:29 -0500

    Update Functions.py

--- Honors-Thesis/Functions.py
+++ Honors-Thesis/Functions.py
@@ -1,85 +1,86 @@
 class verilogFuncs:
-    def  parser(f): #function to parse the verilog file to create a word array
+    def  parser(file): #function to parse the verilog file to create a word array
         wordArr = [] #an array of characters that appear in sequence
         wordArrsave = []
         count = 0
         index = 0
-        for line in f:
-            for word in line:
-                if word == ' ' or word == '\n' or word == '	' or word==',': #creating a line of demarkation to seperate characters in sequence
-                    command = ''.join(wordArr) #joining those characters into a str word that represents some kind of command
-                    check = 0
-                    if "~" in command:
-                        check = 1
-                        if command in wordArrsave:
-                            wordArrsave.remove(command)
-                            index -=1
-                        command = command.replace('~',"")
-                        wordArrsave.append('~')
-                        index+=1
-                        wordArrsave.append(command)
-                        index+=1
-                    if '(' in command:
-                        check = 1
-                        if command in wordArrsave:
-                            wordArrsave.remove(command)
-                            index -=1
-                        command = command.replace('(',"")
-                        wordArrsave.append('(')
-                        index+=1
-                        wordArrsave.append(command)
-                        index+=1
-                    if '!' in command:
-                        check = 1
-                        if command in wordArrsave:
-                            wordArrsave.remove(command)
-                            index -=1
-                        command = command.replace('!',"")
-                        wordArrsave.append('!')
-                        index+=1
-                        wordArrsave.append(command)
-                        index+=1
-                    if '//' in command:
-                        check =1
-                        if command in wordArrsave:
-                            wordArrsave.remove(command)
-                            index -=1
-                        command = command.replace('//',"")
-                        wordArrsave.append('//')
-                        index +=1
-                        wordArrsave.append(command)
-                        index +=1
-                    if ')' in command:
-                        check =1
-                        if command in wordArrsave:
-                            wordArrsave.remove(command)
-                            index -=1
-                        command = command.replace(')',"")
-                        wordArrsave.append(command)
-                        index +=1
-                        wordArrsave.append(')')
-                        index +=1
-                    if ';' in command:
-                        check =1
-                        if command in wordArrsave:
-                            wordArrsave.remove(command)
-                            index -=1
-                        command = command.replace(';',"")
-                        wordArrsave.append(command)
-                        if wordArrsave[index-1] !=None and wordArrsave[index-1] == ")":
-                            wordArrsave[index] = ")"
-                            wordArrsave[index-1]=command
-                        index+=1
-                        wordArrsave.append(';')
-                        index+=1
+        with open(file, 'r') as f:
+            for line in f:
+                for word in line:
+                    if word == ' ' or word == '\n' or word == '	' or word==',': #creating a line of demarkation to seperate characters in sequence
+                        command = ''.join(wordArr) #joining those characters into a str word that represents some kind of command
+                        check = 0
+                        if "~" in command:
+                            check = 1
+                            if command in wordArrsave:
+                                wordArrsave.remove(command)
+                                index -=1
+                            command = command.replace('~',"")
+                            wordArrsave.append('~')
+                            index+=1
+                            wordArrsave.append(command)
+                            index+=1
+                        if '(' in command:
+                            check = 1
+                            if command in wordArrsave:
+                                wordArrsave.remove(command)
+                                index -=1
+                            command = command.replace('(',"")
+                            wordArrsave.append('(')
+                            index+=1
+                            wordArrsave.append(command)
+                            index+=1
+                        if '!' in command:
+                            check = 1
+                            if command in wordArrsave:
+                                wordArrsave.remove(command)
+                                index -=1
+                            command = command.replace('!',"")
+                            wordArrsave.append('!')
+                            index+=1
+                            wordArrsave.append(command)
+                            index+=1
+                        if '//' in command:
+                            check =1
+                            if command in wordArrsave:
+                                wordArrsave.remove(command)
+                                index -=1
+                            command = command.replace('//',"")
+                            wordArrsave.append('//')
+                            index +=1
+                            wordArrsave.append(command)
+                            index +=1
+                        if ')' in command:
+                            check =1
+                            if command in wordArrsave:
+                                wordArrsave.remove(command)
+                                index -=1
+                            command = command.replace(')',"")
+                            wordArrsave.append(command)
+                            index +=1
+                            wordArrsave.append(')')
+                            index +=1
+                        if ';' in command:
+                            check =1
+                            if command in wordArrsave:
+                                wordArrsave.remove(command)
+                                index -=1
+                            command = command.replace(';',"")
+                            wordArrsave.append(command)
+                            if wordArrsave[index-1] !=None and wordArrsave[index-1] == ")":
+                                wordArrsave[index] = ")"
+                                wordArrsave[index-1]=command
+                            index+=1
+                            wordArrsave.append(';')
+                            index+=1
 
-                    if check == 0:
-                        wordArrsave.append(command) #appending that command to my list of words that I will refer back to later
-                        index +=1
-                    wordArr = []
-                    count +=1
-                else:
-                    wordArr.append(word)
+                        if check == 0:
+                            wordArrsave.append(command) #appending that command to my list of words that I will refer back to later
+                            index +=1
+                        wordArr = []
+                        count +=1
+                    else:
+                        wordArr.append(word)
 
         #-----------------------Preparing the command array to leave only relevant information from the verilog file ----------------------------------------
         while '' in wordArrsave: #removing empty strings from the list
@@ -109,7 +110,7 @@ class verilogFuncs:
                         arrays[index2] = items.replace("(","")
                     if ")" in items:
                         arrays[index2] = items.replace(")","")       
-    def writeVisuals(fDot,operationsA,bufferA,labelon):
+    def writeVisuals(fDot,operationsA,bufferA,labelon,inputon):
         savedArr = []
         dotfile = prepareDot(fDot)
         for arrays in operationsA:
@@ -124,21 +125,32 @@ class verilogFuncs:
                 if items in bufferA:
                     for itemx in operationsA:
                         if itemx[2] == items:
-                            if labelon ==1:
+                            if labelon ==1 :
                                 if f"{itemx[0],arrays[0],items}" not in savedArr:
                                     savedArr.append(f"{itemx[0],arrays[0],items}")
                                     dotfile.writeEdgeNode(itemx[0],arrays[0],items)
-                            else:
+                            if labelon == 0 and inputon == 0:
                                 if f"{itemx[0],arrays[0]}" not in savedArr:
                                     savedArr.append(f"{itemx[0],arrays[0]}")
                                     dotfile.writeEdgeNode(itemx[0],arrays[0])
+                            if inputon == 1 and labelon == 0:
+                                if f"{itemx[0],arrays[0],items}" not in savedArr:
+                                    savedArr.append(f"{itemx[0],arrays[0],items}")
+                                    dotfile.writeEdgeNode(itemx[0],arrays[0],operationsA[index][3][arrays[1].index(items)])
                 else:
-                    if f"{items[0],arrays[0]}" not in savedArr:
+                    if f"{items[0],arrays[0]}" not in savedArr and inputon==1:
+                        savedArr.append(f"{items[0],arrays[0]}")
+                        dotfile.writeEdgeNode(items,arrays[0],operationsA[index][3][operationsA[index][1].index(items)])
+                    elif f"{items[0],arrays[0]}" not in savedArr and inputon==0:
                         savedArr.append(f"{items[0],arrays[0]}")
                         dotfile.writeEdgeNode(items,arrays[0])
             if arrays[2] not in bufferA:
-                if f"{arrays[0],arrays[2]}":
+                if f"{arrays[0],arrays[2]}" and inputon == 1:
+                    dotfile.writeEdgeNode(arrays[0],arrays[2],operationsA[index][4][0])
+                elif f"{arrays[0],arrays[2]}" and inputon == 0:
                     dotfile.writeEdgeNode(arrays[0],arrays[2])
+    def andGate(inputA,inputB):
+        return str(int(inputA)*int(inputB))
 class prepareDot:
     def __init__(self,fDot):
         self.fDot = fDot
@@ -173,7 +185,7 @@ class prepareDot:
         if label == "":
             self.fDot.write(f'"{inN}" -> "{outN}" \n')
         else:
-            self.fDot.write(f'"{inN}" -> "{outN}" [label="{label}"]\n')
+            self.fDot.write(f'"{inN}" -> "{outN}" [label="{label}"] [color="red"]\n')
     def createSubgraph(self,input):
         self.fDot.write("subgraph{\nrand=same;")
         for item in input:
@@ -200,3 +212,26 @@ class prepareDot:
         return locs
     def endFile(self):
         self.fDot.write("}")
+
+class simulationInput:
+
+    def and_gate(a, b):
+        return int(a and b)
+    def not_gate(a):
+        return int(not a)
+    def nor_gate(a, b):
+        return int(not (a or b))
+    def xor_gate(a, b):
+        return int(a != b)
+    def xnor_gate(a, b):
+        return int(a == b)
+    def nand_gate(a, b):
+        return int(not (a and b))
+    def or_gate(a, b):
+        return int(a or b)
+
+
+
+
+
+
